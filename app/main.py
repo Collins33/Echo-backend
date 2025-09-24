2b555ae3df664f1b2681e88f3db5b794233f62ec
collins njau <collinsnjau39@gmail.com>
2025-09-24 16:08:16 +0300

    Add Text to speech service

--- app/main.py
+++ app/main.py
@@ -1,15 +1,23 @@
 from fastapi import FastAPI, UploadFile, File, Form
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+from app.services.text_to_speech import TextToSpeech
+
+
 from app.services.whisper_asr import WhisperASR
+
 import os
+import io
 
 app = FastAPI(title="Echo Backend")
 asr_model = WhisperASR()
 
+class TTSRequest(BaseModel):
+    text: str
 
 @app.get("/")
 def root():
     return {"message": "Echo backend is running"}
-
 
 @app.post("/transcribe")
 async def transcribe_audio(file: UploadFile = File(...)) :
@@ -23,3 +31,15 @@ async def transcribe_audio(file: UploadFile = File(...)) :
     finally:
         if os.path.exists(temp_path):
             os.remove(temp_path)
+
+@app.post("/tts")
+def generate_tts(request: TTSRequest):
+    tts = TextToSpeech(request.text)
+    mp3_path = tts.speak()
+    return FileResponse(
+        mp3_path,
+        media_type="audio/mpeg",
+        filename="speech.mp3"
+    )
+
+
